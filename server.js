d9d44e7f109285bfed57535dc2ebedb272759dd3
spinfoo <jacobo@defensahacker.com>
2019-10-10 12:42:31 +0200

    Added CSS to the XSS

--- server.js
+++ server.js
@@ -19,22 +19,14 @@ app.get('/', (req, res) => {
   res.set("X-XSS-Protection", "0");
   var name = req.query.name;
   console.log("Received payload: "+name);
-  res.send(" <!doctype html>" + 
-    "<html>" + 
-    "<body>" + 
-    "<h1>XSS test - GET</h1>" + 
-    'Hello ' + name + '\n' + 
-    "<form method=post><input style=\"width: 600px; height: 30px;\" type=text name=p value=\"user input\"/><br/><input type=submit value=Send></form>" + 
-    "</body>" + 
-    "</html>"
-    );
+  res.send(get_request(name));
 });
 
 app.post('/', (req, res) => {
   res.set("X-XSS-Protection", "0");
   var name = req.body.p;
   console.log("Received payload: "+name);
-  res.send("<h1>XSS test - POST</h1>Text received:<br/><textarea rows=5 cols=50>"+name+"</textarea>");
+  res.send(get_reply(name));
 });
 
 app.get('/patched', (req, res) => {
@@ -42,15 +34,7 @@ app.get('/patched', (req, res) => {
   console.log("Received payload: "+req.query.name);
   var name = sanitize(req.query.name, {whiteList: []});
   console.log("Sanitized payload: "+name);
-  res.send(" <!doctype html>" + 
-    "<html>" + 
-    "<body>" + 
-    "<h1>XSS test - GET</h1>" + 
-    'Hello ' + name + '\n' + 
-    "<form method=post><input style=\"width: 600px; height: 30px;\" type=text name=p value=\"user input\"/><br/><input type=submit value=Send></form>" + 
-    "</body>" + 
-    "</html>"
-    );
+  res.send(get_request(name));
 });
 
 app.post('/patched', (req, res) => {
@@ -58,10 +42,65 @@ app.post('/patched', (req, res) => {
   console.log("Received payload: "+req.body.p);
   var name = sanitize(req.body.p, {whiteList: []});
   console.log("Sanitized payload: "+name);
-  res.send("<h1>XSS test - POST</h1>Text received:<br/><textarea rows=5 cols=50>"+name+"</textarea>");
+  res.send(get_reply(name));
 });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
+
+function get_request(name) {
+	return "<!DOCTYPE html>\n" + 
+  "<html>\n" + 
+  "<head>\n" +
+    '<meta charset="utf-8">\n' +
+    '<meta name="viewport" content="width=device-width, initial-scale=1">\n' +
+    '<title>XSS labs</title>\n' +
+    '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">\n' +
+    '<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>\n' +
+  '</head>\n' +
+  "<body>\n" + 
+  	"<div class='container'><section class='section'>\n" +
+    "<h1 class='title'XSS test - GET</h1>\n\n\n" + 
+    'Hello ' + name + '\n\n\n' + 
+    "<form method=post>\n" + 
+    '<div class="field">\n' +
+    '<div class="control">\n' +
+    "<input class='input is-danger' style=\"width: 600px; height: 30px;\" type=text name=p value=\"user input\"/>\n" + 
+    '</div>\n' +
+    '</div>\n' +
+    "<br/>\n<input class='button is-danger' type=submit value=Send>\n" + 
+    "</form>\n" + 
+    "<hr/>\n" +
+    '<h2 class="subtitle">Some examples (GET method):</h2>' +
+    "<a href='https://xss-lab5452.appspot.com/?name=World'>https://xss-lab5452.appspot.com/?name=World</a><br/>\n" +
+    "<a href='https://xss-lab5452.appspot.com/?name=<b>World</b>'>https://xss-lab5452.appspot.com/?name=&lt;b&gt;World&lt;/b&gt;</a>" +
+    "<br/><br/>" + 
+    '<h2 class="subtitle">Patched version (GET method):</h2>' +
+    "<a href='https://xss-lab5452.appspot.com/patched?name=World'>https://xss-lab5452.appspot.com/patched?name=World</a><br/>\n" +
+    "<a href='https://xss-lab5452.appspot.com/patched?name=<b>World</b>'>https://xss-lab5452.appspot.com/patched?name=&lt;b&gt;World&lt;/b&gt;</a>" +
+    "</section></div>" +
+   "</body>\n" + 
+   "</html>";
+ }
+
+function get_reply(name) {
+	return "<!DOCTYPE html>\n" + 
+  "<html>\n" + 
+  "<head>\n" +
+    '<meta charset="utf-8">\n' +
+    '<meta name="viewport" content="width=device-width, initial-scale=1">\n' +
+    '<title>XSS labs</title>\n' +
+    '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">\n' +
+    '<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>\n' +
+  '</head>\n' +
+  "<body>\n" + 
+  	"<div class='container'><section class='section'>\n" +
+  	"<h1 class='title'>XSS test - POST</h1>\n" + 
+  	"Text received:<br/>\n\n\n" + 
+  	"<textarea class='textarea' rows=5 cols=50>"+name+"</textarea>\n\n\n" +
+  	"</section></div>" +
+   "</body>\n" + 
+   "</html>";
+}
